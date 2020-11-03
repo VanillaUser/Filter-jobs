72cc2149936e857797f4f7a95583dbb05e26495c
Jeremy <jeremy.rodriguezp@yahoo.com>
2020-11-03 14:19:52 -0500

    response jobs cards

--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -9,9 +9,14 @@ export class JobsComponent implements OnInit {
 
   @Input() job: any;
 
+  mobile = false;
+
   constructor() { }
 
   ngOnInit(): void {
+    if (window.screen.width === (320)) { // 768px portrait
+      this.mobile = true;
+    }
   }
 
 }
